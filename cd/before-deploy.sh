de1c924480cc8e74d96d9fff20ea41ba7074a949
cherrera2001 <chris.herrera07@gmail.com>
2017-10-30 11:57:01 -0500

    fixing error with key name

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_746c7c18da90_key -iv $encrypted_746c7c18da90_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_746c7c18da90_key -iv $encrypted_746c7c18da90_iv -in cd/codeSigningKey.asc.enc -out cd/codeSigningKey.asc -d
     gpg --fast-import cd/codeSigningKey.asc
 fi
